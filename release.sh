bd522e1064f7ebd86effcd0bc205dc3752ac6f6b
Tony Grosinger <tony@grosinger.net>
2020-10-30 07:14:22 -0700

    Remove unncessary release.sh

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf table-editor-obsidian
-mkdir table-editor-obsidian
-cp README.md main.js manifest.json table-editor-obsidian
-zip -r table-editor-obsidian_v${1}.zip table-editor-obsidian
